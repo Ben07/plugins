db1a5aa4b3235b1094afc17967e504acd1e43714
ben07 <287971237@qq.com>
2015-02-05 14:25:12 +0800

    modify clear

--- src/ClearPlugin.java
+++ src/ClearPlugin.java
@@ -8,35 +8,73 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.content.Context;
+import android.os.Environment;
 
-public class ClearPlugin  extends CordovaPlugin{
+public class ClearPlugin extends CordovaPlugin {
 	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if("clean".equals(action)){
-			deleteCache(webView.getContext());
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+		if ("clear".equals(action)) {
+			Context context = webView.getContext();
+			cleanInternalCache(context);
+			cleanExternalCache(context);
+			cleanDatabases(context);
+			cleanSharedPreference(context);
+			cleanFiles(context);
 			return true;
 		}
 		return false;
 	}
-	public void deleteCache(Context context) {
-	    try {
-	        File dir = context.getCacheDir();
-	        if (dir != null && dir.isDirectory()) {
-	            deleteDir(dir);
-	        }
-	    } catch (Exception e) {}
+
+	public void cleanInternalCache(Context context) {
+		deleteFilesByDirectory(context.getCacheDir());
 	}
 
-	public boolean deleteDir(File dir) {
-	    if (dir != null && dir.isDirectory()) {
-	        String[] children = dir.list();
-	        for (int i = 0; i < children.length; i++) {
-	            boolean success = deleteDir(new File(dir, children[i]));
-	            if (!success) {
-	                return false;
-	            }
-	        }
-	    }
-	    return dir.delete();
+	public void cleanDatabases(Context context) {
+		deleteFilesByDirectory(new File("/data/data/"
+				+ context.getPackageName() + "/databases"));
+	}
+
+	public void cleanSharedPreference(Context context) {
+		deleteFilesByDirectory(new File("/data/data/"
+				+ context.getPackageName() + "/shared_prefs"));
+	}
+
+	public void cleanDatabaseByName(Context context, String dbName) {
+		context.deleteDatabase(dbName);
+	}
+
+	public void cleanFiles(Context context) {
+		deleteFilesByDirectory(context.getFilesDir());
+	}
+
+	public void cleanExternalCache(Context context) {
+		if (Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED)) {
+			deleteFilesByDirectory(context.getExternalCacheDir());
+		}
+	}
+
+	public void cleanCustomCache(String filePath) {
+		deleteFilesByDirectory(new File(filePath));
+	}
+
+//	public void cleanApplicationData(Context context, String... filepath) {
+//		cleanInternalCache(context);
+//		cleanExternalCache(context);
+//		cleanDatabases(context);
+//		cleanSharedPreference(context);
+//		cleanFiles(context);
+//		for (String filePath : filepath) {
+//			cleanCustomCache(filePath);
+//		}
+//	}
+
+	private static void deleteFilesByDirectory(File directory) {
+		if (directory != null && directory.exists() && directory.isDirectory()) {
+			for (File item : directory.listFiles()) {
+				item.delete();
+			}
+		}
 	}
 }
